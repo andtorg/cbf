3cd19a1504b274bc1bf633b60bd3c3d9ce76fe69
Andrea Torre <andtorg@gmail.com>
2013-07-22 21:31:12 +0200

    update

--- ctools-downloader.sh
+++ ctools-downloader.sh
@@ -5,16 +5,20 @@ echo
 echo v0.1 still developing 
 
 BASE_DIR="pentaho-addons"
+TEMP_DIR=$BASE_DIR/tmp
+
 VERSION_DIR=$1
 
+
 rm -rf $BASE_DIR/tmp
-mkdir $BASE_DIR/tmp
+mkdir $TEMP_DIR
 
 
 downloadCDF(){
 	echo "downloading CDF..."
 	URL='http://ci.analytical-labs.com/job/Webdetails-CDF-release/lastSuccessfulBuild/artifact/bi-platform-v2-plugin/dist/*zip*/dist.zip'
-	wget -P $BASE_DIR/tmp $URL 
+	wget -P $TEMP_DIR $URL
+	unzip $TEMP_DIR/dist.zip -d $TEMP_DIR/cdf-tmp #MODIFIXARE: NON FUNZIONA 
 	#rm -f .tmp/dist/marketplace.xml
 	#unzip .tmp/dist/dist.zip -d .tmp > /dev/null
 	echo "CDF downloaded!"
