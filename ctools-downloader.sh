6e7e0de59c07fafcf6eebc9d6b380d314ac06361
Andrea Torre <andtorg@gmail.com>
2013-07-24 16:19:44 +0200

    added cde downloading

--- ctools-downloader.sh
+++ ctools-downloader.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#NB create setupsamples during install
+
 echo CHANGELOG:
 echo
 echo v0.1 still developing 
@@ -19,12 +21,25 @@ downloadCDF(){
 	URL='http://ci.analytical-labs.com/job/Webdetails-CDF-release/lastSuccessfulBuild/artifact/bi-platform-v2-plugin/dist/*zip*/dist.zip'
 	wget -P $TEMP_DIR $URL
 	unzip -j $TEMP_DIR/dist.zip "dist/pentaho-cdf-"$VERSION_DIR".zip" -d $BASE_DIR/$VERSION_DIR/cdf
+	unzip -j $TEMP_DIR/dist.zip "dist/pentaho-cdf-samples-"$VERSION_DIR".zip" -d $BASE_DIR/$VERSION_DIR/cdf
+	rm $TEMP_DIR/dist.zip
 	echo "CDF downloaded!"
 }
 
+downloadCDE(){
+	echo "downloading CDE..."
+	URL='http://ci.analytical-labs.com/job/Webdetails-CDE-release/lastSuccessfulBuild/artifact/server/plugin/dist/*zip*/dist.zip'
+	wget -P $TEMP_DIR $URL
+	unzip -jo $TEMP_DIR/dist.zip "dist/pentaho-cdf-dd-"$VERSION_DIR".zip" -d $BASE_DIR/$VERSION_DIR/cde
+	unzip -jo $TEMP_DIR/dist.zip "dist/pentaho-cdf-dd-solution-"$VERSION_DIR".zip" -d $BASE_DIR/$VERSION_DIR/cde
+	rm $TEMP_DIR/dist.zip
+	echo "CDE downloaded!"
+}
+
+
 downloadAll(){
 	downloadCDF;
-
+	downloadCDE;
 }
 
 if [ ! -d $BASE_DIR/$VERSION_DIR ]
